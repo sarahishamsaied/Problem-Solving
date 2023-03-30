9ce6b608328e20adfe50ae719e364c4cbdf64e87
sarahishamsaied <sarahishamsaied@gmail.com>
2023-03-30 21:48:56 +0200

    surounded scanner with try block

--- Code Forces/StonesOnTheTable.java
+++ Code Forces/StonesOnTheTable.java
@@ -1,15 +1,16 @@
 import java.util.*;
-public class Main{
+class Solution{
         public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        String s = input.next();
-        int c = 0,b=0;
-        for(int i = 0 ; i< s.length() ; i++){
-            if (s.charAt(i)==b)
-                c++;
-            b = s.charAt(i);
+        try (Scanner input = new Scanner(System.in)) {
+            int n = input.nextInt();
+            String s = input.next();
+            int c = 0,b=0;
+            for(int i = 0 ; i< s.length() ; i++){
+                if (s.charAt(i)==b)
+                    c++;
+                b = s.charAt(i);
+            }
+            System.out.println(c);
         }
-        System.out.println(c);
     }
 }
--- Code Forces/Team.java
+++ Code Forces/Team.java
@@ -1,18 +1,19 @@
 import java.util.*;
-public class Main {
+class Main {
         public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int x,imp = 0;
-        for(int i = 0;i<n;i++){
-            int sol = 0;
-            for(int j = 0 ;j<3;j++){
-              x = input.nextInt();
-              sol+=x;
-            }
-            if(sol>=2)
-               imp++;
+        try (Scanner input = new Scanner(System.in)) {
+          int n = input.nextInt();
+          int x,imp = 0;
+          for(int i = 0;i<n;i++){
+              int sol = 0;
+              for(int j = 0 ;j<3;j++){
+                x = input.nextInt();
+                sol+=x;
+              }
+              if(sol>=2)
+                 imp++;
+          }
+          System.out.println(imp);
         }
-        System.out.println(imp);
     }
 }
