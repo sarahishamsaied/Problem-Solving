9e7e474a1da250cc78c5d920c82c8f746baa0b31
sarahishamsaied <sarahishamsaied@gmail.com>
2023-03-31 17:55:59 +0200

    surrounded with try block

--- Code Forces/BeautifulMatrix.java
+++ Code Forces/BeautifulMatrix.java
@@ -1,21 +1,22 @@
 import java.util.*;
-public class Main {
+ class Main {
         public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int col = 0,row = 0;
-        int[][] arr = new int[10][20];
-        int x;
-        for (int i =0 ;i<5;i++){
-            for(int j =0;j<5;j++){
-                arr[i][j] = input.nextInt();
-                if(arr[i][j] == 1){
-                    row = i;
-                    col = j;
-                }
+        try (Scanner input = new Scanner(System.in)) {
+            int col = 0,row = 0;
+            int[][] arr = new int[10][20];
+            int x;
+            for (int i =0 ;i<5;i++){
+                for(int j =0;j<5;j++){
+                    arr[i][j] = input.nextInt();
+                    if(arr[i][j] == 1){
+                        row = i;
+                        col = j;
+                    }
  
+                }
             }
+            x = Math.abs(2 - row) + Math.abs(2-col);
+            System.out.println(x);
         }
-        x = Math.abs(2 - row) + Math.abs(2-col);
-        System.out.println(x);
     }
 }
